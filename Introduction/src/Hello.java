a8e6f7dd5c134b0150efc10b0bb61c9b79ec0975
GaurangGawade <[gauranggawade777@gmail.com]>
2024-07-31 16:14:39 +0530

    commit

---
+++ Introduction/src/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		System.out.println("Hi,How are you???");
+		System.out.println("I am fine, What about you???");
+
+	}
+
+}
